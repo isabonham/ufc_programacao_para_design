17d1d1fe5e3944d937b9fc4d1175aae92f99a942
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 15:32:47 -0300

    atualizando descrição para avaliação

--- Carro/Carro.ts
+++ Carro/Carro.ts
@@ -1,3 +1,12 @@
+/*
+O QUE APRENDI: o que são atributos e para que servem os métodos;
+COM QUEM APRENDI: com o professor durante a aula;
+COMO APRENDI: prestando atenção no que o professor tava ensinando;
+
+TEMPO: 1H 30min
+*/
+
+
 class Carro {
     public pass: number;
     public passMax: number;
