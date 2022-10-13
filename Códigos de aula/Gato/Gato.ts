1cd18b0b9b4e4a2e6e340c3cf25011a35abfcc21
Isabela Vieira <isa.bonham@gmail.com>
2022-10-13 16:24:26 -0300

    Create Gato.ts

---
+++ Códigos de aula/Gato/Gato.ts
@@ -0,0 +1,55 @@
+
+class Pet {
+    private nome: string;
+    private tipo: string;
+    public constructor(nome: string, tipo: string) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    public getTipo(): string {
+        return this.tipo;
+    }
+    public setTipo(value: string) {
+        this.tipo = value;
+    }
+    public getNome(): string {
+        return this.nome;
+    }
+    public setNome(value: string) {
+        this.nome = value;
+    }
+    public toString(): string {
+        return `${this.nome}:${this.tipo}`;
+    }
+}
+
+function main() {
+    let sacola: Array<Pet> = new Array();
+    sacola.push(new Pet("chaninha", "gato"));
+    sacola.push(new Pet("garfield", "gato"));
+    sacola.push(new Pet("pluto", "cachorro"));
+    sacola.push(new Pet("tom", "gato"));
+    sacola.push(new Pet("sabrina", "humana"));
+
+    let i = 0;
+    while (i < sacola.length) {
+        if (sacola[i].getTipo() !== "gato") {
+            sacola.splice(i, 1);
+        } else {
+            i += 1;
+        }
+    }
+
+    let sacola_gatos = sacola
+        .filter(elem => elem.getTipo() == "gato")
+        .map(elem => elem.getNome());
+    
+    let nomes_gatos: string[] = [];
+    for (let elem of sacola) {
+        if (elem.getTipo() == "gato")
+            nomes_gatos.push(elem.getNome());
+    }
+    
+}
+
+main()
