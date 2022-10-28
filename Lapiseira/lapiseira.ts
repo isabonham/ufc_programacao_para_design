74021e93b87b8ef1ad7cc6efdcc68731d557fbc1
Isabela Vieira <isa.bonham@gmail.com>
2022-10-28 15:52:58 -0300

    Update lapiseira.ts

--- Lapiseira/lapiseira.ts
+++ Lapiseira/lapiseira.ts
@@ -3,14 +3,10 @@ class Lead {
     private hardness: string;
     private size: number;
 
-    public constructor(calibre: number, dureza:string, tamanho: number) {
+    public constructor(calibre: number, dureza: string, tamanho: number) {
         this.thickness = calibre;
         this.hardness = dureza;
         this.size = tamanho;
-    }
-
-    public toString(): string {
-        return `${this.thickness}:${this.hardness}:${this.size}`;
     }
 
     public usagePerSheet(): number {
@@ -42,7 +38,11 @@ class Lead {
     }
 
     public setSize(size: number): void {
-        this.size = size;
+        this.size += size;
+    }
+
+    public toString(): string {
+        return `${this.thickness}:${this.hardness}:${this.size}`;
     }
 }
 
@@ -53,31 +53,93 @@ class Pencil {
 
     public constructor(thickness: number) {
         this.thickness = thickness;
+        this.tip = null;
     }
 
-    public insert(lead: Lead): boolean {
-        return true
+    public insert(grafite: Lead): boolean {
+        if (grafite.getThickness() > this.thickness) {
+            console.log ("fail: calibre incompatível");
+            return false;
+        }
+        if (this.tip !== null) {
+            console.log ("teste, o bico está cheio então vou inserir no tambor")
+            this.barrel.push(new Lead(grafite.getThickness(), grafite.getHardness(), grafite.getSize()));
+        }
+        this.tip = new Lead(grafite.getThickness(), grafite.getHardness(), grafite.getSize());
+        return true;
     }
 
     public remove(): Lead | null {
-        return null;
+        this.tip = null;
+        return this.tip;
     }
 
     public pull(): boolean {
         return true
     }
 
-    public writePage(): void { //todo
-
+    writePage(): void {
+        if (this.tip === null) {
+            console.log ("fail: nao existe grafite");
+        }
+        else if (this.tip.getSize() <= 10) {
+            console.log ("fail: tamanho insuficiente");
+        }
+        else {
+            if (this.tip.getHardness() === "HB") {
+                if (this.tip.getSize() - 1 < 10) {
+                    let x = 10 - this.tip.getSize();
+                    this.tip.setSize(x);
+                    console.log ("fail: folha incompleta");
+                }
+                else {
+                    this.tip.setSize(-1);
+                }
+            }
+            else if (this.tip.getHardness() === "2B") {
+                if (this.tip.getSize() - 2 < 10) {
+                    let x = 10 - this.tip.getSize();
+                    this.tip.setSize(x);
+                    console.log ("fail: folha incompleta");
+                }
+                else {
+                    this.tip.setSize(-2);
+                }
+            }
+            else if (this.tip.getHardness() === "4B") {
+                if (this.tip.getSize() - 4 < 10) {
+                    let x = 10 - this.tip.getSize();
+                    this.tip.setSize(x);
+                    console.log ("fail: folha incompleta");
+                }
+                else {
+                    this.tip.setSize(-4);
+                }
+            }
+            else if (this.tip.getHardness() === "6B") {
+                if (this.tip.getSize() - 6 < 10) {
+                    let x = 10 - this.tip.getSize();
+                    this.tip.setSize(x);
+                    console.log ("fail: folha incompleta");
+                }
+                else {
+                    this.tip.setSize(-6);
+                }
+            }
+        }
     }
-    public toString(): string {
-        let saida =  "calibre: " + this.thickness + ", bico: " +
-                (this.tip != null ? "[" + this.tip + "]" : "[]") + ", tambor: {";
-        for(let g of this.barrel) {
-            saida += "[" + g + "]";
-        }  
-        saida += "}";
 
-        return saida;
+    public toString(): string {
+        let response =  "calibre: " + this.thickness +
+                        ", bico: " + (this.tip != null ? "[" + this.tip + "]" : "[]") +
+                        ", tambor: {";
+
+        for(let g of this.barrel) {
+            response += "[" + g + "]";
+        }
+
+        response += "}";
+
+        return response;
     }
 }
