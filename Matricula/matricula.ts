40167168888ebcfeb59e4561690432fcd5a2e8f3
Isabela Vieira <isa.bonham@gmail.com>
2022-11-17 16:33:27 -0300

    Create matricula.ts

---
+++ Matricula/matricula.ts
@@ -0,0 +1,51 @@
+class Aluno {
+    private id: string;
+    private discps: Map<string, Discp>;
+
+    public getId() {
+        return this.id;
+    }
+
+    public addDiscp(discp: Discp) {
+        if (this.discps.has(discp.getId())) {
+            return;
+        }
+        this.discps.set(discp.getId(), discp);
+        discp.addAluno(this);
+    }
+
+    public rmDiscp(discp: string) {
+        let d = this.discps.get(discp) 
+        if (d === undefined) {
+            return;           
+        }
+        this.discps.delete(discp);
+        d.rmAluno(this.id);
+    }
+}
+
+class Discp {
+    private id: string;
+    private alunos: Map<string, Aluno>;
+
+    public getId() {
+        return this.id;
+    }
+
+    public addAluno (aluno: Aluno) {
+        if (this.alunos.has(aluno.getId())) {
+            return;
+        }
+        this.alunos.set(aluno.getId(), aluno);
+        aluno.addDiscp(this);
+    }
+
+    public rmAluno(aluno: string) {
+        let a = this.alunos.get(aluno) 
+        if (a === undefined) {
+            return;           
+        }
+        this.alunos.delete(aluno);
+        a.rmDiscp(this.id);
+    }
+}
