35e5d50814c0517e5ca5a5ea8e3e4b49d459fdb7
Isabela Vieira <isa.bonham@gmail.com>
2022-12-13 17:25:03 -0300

    Update tt.ts

--- Twitter/tt.ts
+++ Twitter/tt.ts
@@ -1,259 +1,281 @@
 class User {
-    private username: string
-    private inbox: Inbox
-    private following: Map<string, User>
-    private followers: Map<string, User>
-    constructor (name: string) {
-        this.username = name
-        this.inbox = new Inbox
-        this.following = new Map<string, User>()
-        this.followers = new Map<string, User>()  
+    private username: string;
+    private inbox: Inbox;
+    private following: Map<string, User>;
+    private followers: Map<string, User>;
+
+    public constructor(name: string) {
+        this.username = name;
+        this.inbox = new Inbox;
+        this.following = new Map<string, User>();
+        this.followers = new Map<string, User>();
     }
 
-    getUsername() {
+    public getUsername() {
         return this.username;
     }
 
-    getFollowers() {
-        return this.followers.keys()
+    public getFollowers() {
+        return this.followers.keys();
     }
 
-    getFollowing() {
-        return this.following.keys()
+    public getFollowing() {
+        return this.following.keys();
     }
 
-    getTimeline() {
-        return this.inbox.getTimeline()
+    public getTimeline() {
+        return this.inbox.getTimeline();
     }
 
-    getInbox() {
-        return this.inbox
+    public getInbox() {
+        return this.inbox;
     }
 
-    follow(seguir: User) {
-        if (this.username == seguir.getUsername()){
-            return console.log ("você não pode se seguir!")
+    public follow(user: User) {
+        if(this.username === user.getUsername()) {
+            console.log("fail: você não pode se seguir!");
+            return false;
         }
-        this.following.set(seguir.getUsername(), seguir)
-        seguir.followers.set(this.getUsername(), this)
+        this.following.set(user.getUsername(), user);
+        user.followers.set(this.getUsername(), this);
     }
 
-    unfollow(outro: string) {
-        if (!this.following.has(outro)) {
-            return console.log("Usuário não encontrado na timeline")
+    public unfollow(user: string) {
+        if (!this.following.has(user)) {
+            return console.log("Usuário não encontrado na timeline");
         }
-        let deleta = this.following.get(outro)
-        if (deleta !== undefined) {
-           deleta.followers.delete(this.getUsername())
-            this.following.delete(outro) 
-            this.inbox.rmvMsg(outro)
+        let d = this.following.get(user);
+        if (d !== undefined) {
+            d.followers.delete(this.getUsername());
+            this.following.delete(user) ;
+            this.inbox.rmvMsg(user);
         }
     }
 
-    sendTwitada(twitada: Tweet) {
-        this.inbox.guardarNaTimeline(twitada)
-        this.inbox.guardarNosMeusTweets(twitada)
+    public twittar(tweet: Tweet) {
+        this.inbox.Timeline(tweet);
+        this.inbox.Tweets(tweet);
         for (let seguidores of this.followers.values()) {
-            seguidores.inbox.guardarNaTimeline(twitada)
+            seguidores.inbox.Timeline(tweet);
         }
     }
 
-    like(id: number) {
-        let tweet = this.inbox.getTweet(id)
-        tweet.like(this.getUsername())
+    public like (id: number) {
+        let tweet = this.inbox.getTweet(id);
+        tweet.like(this.getUsername());
     }
 
     public unfollowAll() {
         for (let user of this.following.values()) {
-            user.followers.delete(this.getUsername())
-            this.following.delete(user.getUsername())
+            user.followers.delete(this.getUsername());
+            this.following.delete(user.getUsername());
         }
     }
+
     public rejectAll() {
         for (let user of this.followers.values()) {
-            user.following.delete(this.getUsername())
-            this.followers.delete(user.getUsername())
+            user.following.delete(this.getUsername());
+            this.followers.delete(user.getUsername());
         }
     }
 
     public toString() :string {
-        let followers = this.followers.keys()
-        let following = this.following.keys()
-        return `${this.username} \n Seguindo [${[...following].join(", ")}] \n Seguidores [${[...followers].join(", ")}]`
+        let followers = this.followers.keys();
+        let following = this.following.keys();
+        return `${this.username}\n  seguidos   [${[...following].join(", ")}]\n  seguidores [${[...followers].join(", ")}]`;
     }
 
-    showInbox() {      
-        return `Timeline de ${this.username}:\n${this.inbox.toString()}` 
+    public showInbox() {      
+        return `${this.username}\n${this.inbox.toString()}`;
     }
 }
 
 class Tweet {
-    private id: number
-    private sender: string
-    private msg: string
-    private likes: Array<string> 
-    private rt: Tweet | null
-    private deleted: boolean
-    constructor(id:number, sender: string, msg:string) {
-        this.id = id
-        this.sender = sender
-        this.msg = msg
-        this.likes = new Array()
-        this.rt = null
-        this.deleted = false
+    private id: number;
+    private sender: string;
+    private msg: string;
+    private likes: Array<string>;
+    private rt: Tweet | null;
+    private deleted: boolean;
+
+    public constructor(id:number, sender: string, msg:string) {
+        this.id = id;
+        this.sender = sender;
+        this.msg = msg;
+        this.likes = new Array();
+        this.rt = null;
+        this.deleted = false;
     }
 
-    getSender() {
-        return this.sender
+    public getSender() {
+        return this.sender;
     }
 
-   public getId() {
-        return this.id
+    public getId() {
+        return this.id;
     }
 
-    getMsg() {
-        return this.msg
+    public getMsg() {
+        return this.msg;
     }
 
-    getLikes() {
-        return this.likes
+    public getLikes() {
+        return this.likes;
     }
 
-    setDeleted() {
-        this.deleted = true
-        this.msg = "Esse tweet foi deletado"
-        this.sender =""
+    public setDeleted() {
+        this.deleted = true;
+        this.msg = "Esse tweet foi deletado";
+        this.sender = "";
     }
 
-    isDeleted(): boolean {
-        return this.deleted
+    public isDeleted() {
+        return this.deleted;
     }
     
-    like(username: string) {
-        this.likes.push(username)
+    public like(username: string) {
+        this.likes.push(username);
     }
 
-    setRT(rt: Tweet) {
-        this.rt = rt
+    public setRT(rt: Tweet) {
+        this.rt = rt;
     }
 
-    toString() {
-        let tweet = `(${this.id}) ${this.sender}: ${this.msg} (Likes:${[this.getLikes()].join(" , ")})`
-        if (this.rt !== null)
-        tweet += `\n    ~Retweeted: (${this.rt.getId()}) ${this.rt.getSender()}: ${this.rt.getMsg()}`
-        return tweet 
+    public toString() {
+        let likes = ` [${[this.getLikes()].sort().join(", ")}]`;
+        if (this.getLikes().length === 0) {
+            likes = "";
+        }
+        let saida = `${this.id}:${this.sender} (${this.msg})${likes}\n`;
+        // if (this.getLikes().length !== 0) {
+        //     saida += `[${[this.getLikes()].join(", ")}]\n`;
+        // }
+        // if (this.rt !== null)
+        // tweet += `\n    ~Retweeted: (${this.rt.getId()}) ${this.rt.getSender()}: ${this.rt.getMsg()}`
+        return saida; 
     }
 }
 
 class Inbox {
-    private timeline: Map<number, Tweet>
-    private myTweets: Map<number, Tweet>
-    constructor() {
-        this.timeline = new Map<number, Tweet>()
-        this.myTweets = new Map<number, Tweet>()
+    private timeline: Map<number, Tweet>;
+    private myTweets: Map<number, Tweet>;
+
+    public constructor() {
+        this.timeline = new Map<number, Tweet>();
+        this.myTweets = new Map<number, Tweet>();
     }
 
     public getTimeline(): Array<Tweet> {
-        let saida = new Array()
+        let saida = new Array();
         for (let tweets of this.timeline.values()) {
-            if(tweets.isDeleted() == false)
-            saida.push(tweets)
+            if(tweets.isDeleted() === false) {
+                saida.push(tweets);
+            }
         }
-        saida = [...saida].sort((a, b) => b.getId() - a.getId()) 
-        return saida
+        saida = [...saida].sort((a, b) => b.getId() - a.getId());
+        return saida;
     }
 
-    public getTweet(id:number): Tweet{
-        let tweet: undefined | Tweet = this.timeline.get(id)    
-        if (tweet === undefined){
-        throw new Error("Tweet não encontrado")
+    public getTweet(id:number): Tweet {
+        let tweet: undefined | Tweet = this.timeline.get(id);
+        if (tweet === undefined) {
+            throw new Error("Tweet não encontrado");
         }
-        return tweet  
+        return tweet;
     }
     
-    getMyTweets() {
-        return this.myTweets
+    public getMyTweets() {
+        return this.myTweets;
     }
 
-    public guardarNaTimeline(tweet: Tweet) {
+    public Timeline(tweet: Tweet) {
         this.timeline.set(tweet.getId(), tweet)
     }
 
-    public guardarNosMeusTweets(tweet: Tweet) {
+    public Tweets(tweet: Tweet) {
         this.myTweets.set(tweet.getId(), tweet)
     }
 
-    public rmvMsg(usuario: string) { 
+    public rmvMsg(user: string) { 
         for(let tweets of this.timeline.values()) {
-            if (tweets.getSender() == usuario)
-                this.timeline.delete(tweets.getId())
-         }
+            if (tweets.getSender() === user) {
+                this.timeline.delete(tweets.getId());
+            }
+        }
     }
 
-    toString() {
+    public toString() {
         return `${this.getTimeline().join("\n")}`
     }
 }
 
 class Controller {
-    private users: Map<string, User>
-    private nextTweetId: number
-    private tweets: Map<number, Tweet>
-    constructor () {
-        this.users = new Map<string, User>()
-        this.nextTweetId = 0
-        this.tweets = new Map<number, Tweet>()
+    private users: Map<string, User>;
+    private nextTweetId: number;
+    private tweets: Map<number, Tweet>;
+    
+    public constructor () {
+        this.users = new Map<string, User>();
+        this.nextTweetId = 0;
+        this.tweets = new Map<number, Tweet>();
     }
 
     public getUser(user: string): User | undefined {
-        if (user != undefined)
-        return this.users.get(user)
-    }
-
-    public cadastrar(newuser: User) {
-        if (this.users.has(newuser.getUsername())) {
-           return console.log ("Nome já está em uso")
+        if (user !== undefined) {
+            return this.users.get(user);
         }
-        this.users.set(newuser.getUsername(), newuser)
     }
 
-    public sendTwitada(sender: string, msg: string)  {
-        if (!this.users.has(sender)) {
-            return console.log("Usuário inexistente")
+    public cadastrar(newuser: string): boolean {
+        if (this.users.has(newuser)) {
+           console.log ("Nome já está em uso");
+           return false;
         }
-       let tweet: Tweet = this.criarTweet(sender, msg)
-       this.tweets.set(this.nextTweetId, tweet)
-       let quemTwitou = this.users.get(sender)
-       if (quemTwitou != undefined)
-       quemTwitou.sendTwitada(tweet) 
+        let user = new User(newuser);
+        this.users.set(newuser, user);
+        return true;
     }
 
-    public sendRt(sender: string, twId: number, rtMsg: string){
-        let user = this.users.get(sender)
-        if (user == undefined) 
-            throw new Error("Usuário inexistente")
-        let tweet:Tweet = user.getInbox().getTweet(twId)
-        let rt = this.criarTweet(sender, rtMsg)
-        rt.setRT(tweet)
-        user.sendTwitada(rt)
+    public Twittar(user: string, msg: string)  {
+        if (!this.users.has(user)) {
+            return console.log("Usuário inexistente");
+        }
+        let tweet: Tweet = this.createTweet(user, msg);
+        this.tweets.set(this.nextTweetId, tweet);
+        let sender = this.users.get(user);
+        if (sender != undefined) {
+            sender.twittar(tweet);
+        }
+    }
+
+    public rt(sender: string, twId: number, rtMsg: string) {
+        let user = this.users.get(sender);
+        if (user === undefined) {
+            throw new Error("Usuário inexistente");
+        }
+        let tweet:Tweet = user.getInbox().getTweet(twId);
+        let rt = this.createTweet(sender, rtMsg);
+        rt.setRT(tweet);
+        user.twittar(rt);
      }
     
-    public criarTweet(usuario: string, msg: string) : Tweet {
-        this.nextTweetId++
-        let tweet: Tweet = new Tweet(this.nextTweetId, usuario, msg)
-        return tweet 
+    public createTweet(user: string, msg: string): Tweet {
+        let tweet: Tweet = new Tweet(this.nextTweetId, user, msg);
+        this.nextTweetId++;
+        return tweet;
     }
 
     public rmvUser(user: string) {
-        let usuario: undefined | User = this.users.get(user)
-        if (usuario == undefined)
-        throw new Error("Usuário não existe")
-        usuario.unfollowAll()
-        usuario.rejectAll()
-        for(let tweet of usuario.getInbox().getMyTweets().values()){
-             tweet.setDeleted()
+        let usuario: undefined | User = this.users.get(user);
+        if (usuario === undefined) {
+            throw new Error("Usuário não existe");
         }
-        this.users.delete(user)
+        usuario.unfollowAll();
+        usuario.rejectAll();
+        for(let tweet of usuario.getInbox().getMyTweets().values()) {
+             tweet.setDeleted();
+        }
+        this.users.delete(user);
     }
     
     public toString() {
@@ -262,15 +284,94 @@ class Controller {
         for(let twiteiros of this.users.values()) {
             saida += `${twiteiros.toString()}\n`
         }    
-        saida += "\n" 
-        for (let timeline of this.users.values()) {
-            let users = this.users.get(timeline.getUsername())
-            if (users == undefined)
-            throw new Error("Isso nunca vai cair")
-            saida += `${users.showInbox()}\n`
-        
-        }
-       
-        return saida
+        // saida += "\n" 
+        // for (let timeline of this.users.values()) {
+        //     let users = this.users.get(timeline.getUsername())
+        //     // if (users == undefined)
+        //     // throw new Error("Isso nunca vai cair")
+        //     saida += `${users!.showInbox()}\n`
+        // }
+        return saida;
     }
 }
+
+
+
+
+function main() {
+  let chain = new Map();
+  let ui = [];
+  let tt = new Controller();
+
+  chain.set("show", () => print("" + tt));
+  chain.set("add",  () => tt.cadastrar(ui[1]));
+  chain.set("follow", () => {let one = tt.getUser(ui[1])!; let two = tt.getUser(ui[2])!; one.follow(two);});
+  chain.set("twittar", () => {let user = ui[1]; let msg = ""; for (let i = 2; i < ui.length; i++) {msg += ui[i] + " ";} tt.Twittar(user, msg)});
+  chain.set("timeline",   () => {let user = tt.getUser(ui[1]); if (user === undefined) {console.log("fail: usuario nao encontrado")} else {console.log("" + user.getTimeline())}});
+  chain.set("like", () => {let user = tt.getUser(ui[1])!; user.like(+ui[2])});
+  chain.set("unfollow", () => {let user = tt.getUser(ui[1])!; user.unfollow(ui[2])});
+  chain.set("rt", () => {let msg = ""; for (let i = 3; i < ui.length; i++) {msg += ui[i] + " ";} tt.rt(ui[1], +ui[2], msg)});
+  
+  execute(chain, ui);
+}
+
+// ------------ Funções de Leitura --------------------
+
+// Caso não interativo via moodle
+let __lines = require("fs").readFileSync(0).toString().split("\n");
+let input = () => __lines.shift();
+
+// Caso interativo via readline
+// let readline = require("readline-sync")
+// let input = () => readline.question();
+
+// ------------ Funções de Escrita --------------------
+
+let write = text => process.stdout.write("" + text);
+let print = text => console.log(text);
+
+// ------------ Funções de Formatação --------------------
+
+// Função auxiliar para converter de string para vetor
+// "[1,2,3,4]" para [1, 2, 3, 4]
+function to_vet(token) {
+    let size = token.length;
+    let inside = token.substring(1, size - 1);
+    return inside === "" ? [] : inside.split(",").map(x => +x)
+}
+
+//Converte de vetor para string sem inserir os espaços
+//[1, 2, 3, 4] => "[1,2,3,4]"
+function fmt(vet) {
+    return "[" + vet.join(", ") + "]";
+}
+
+// ------------ Funções do Shell --------------------
+
+
+let execute = (chain, ui) => __shell(chain, ui, true);
+let shell   = (chain, ui) => __shell(chain, ui, false);
+
+function __shell(chain, ui, on_moodle) {
+    while (true) {
+        if (!on_moodle)
+            write("$")
+        let line = input();
+        if (on_moodle)
+            print("$" + line);
+            
+        ui.splice(0); //apagar tudo
+        line.split(" ").forEach(x => ui.push(x));
+        
+        let cmd = ui[0];
+        if (cmd == "end") {
+            return;
+        } else if (chain.has(cmd)) {
+            chain.get(cmd)();
+        } else {
+            print("fail: command not found");
+        }
+    }
+}
+
+main();
